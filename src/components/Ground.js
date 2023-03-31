88c54ad37144bbb65344675771820c537b6dbb64
chemokita13 <chemokita13@gmail.com>
2023-03-31 23:27:29 +0200

    ground finished

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { usePlane } from "@react-three/cannon";
+import { GroundTexture } from "../imgs/textures";
 
 function Ground() {
     const [ref] = usePlane(() => ({
-        rotation: [0, 0, 0],
-        position: [0, 0, 0],
+        rotation: [-Math.PI / 2, 0, 0],
+        position: [0, -0.5, 0],
     }));
+
+    GroundTexture.repeat.set(100,100)
+    
     return (
         <mesh ref={ref}>
             <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <meshStandardMaterial attach="material" color="green" />
+            <meshStandardMaterial attach="material" map={GroundTexture}/>
         </mesh>
     );
 }
